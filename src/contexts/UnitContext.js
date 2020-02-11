adbd18b7f76889cf8094d0b90d455453f85fee8d
Glenn McClanan <producersama@gmail.com>
2020-02-10 19:38:00 -0500

    Work on Unit Context

--- src/contexts/UnitContext.js
+++ src/contexts/UnitContext.js
@@ -4,7 +4,44 @@ import uuid from 'uuid/v1';
 
 export const UnitContext = createContext();
 
-const MovieContextProvider = (props) => {
+const UnitContextProvider = (props) => {
+
+    const [Units, setUnits] = useState([
+        {
+            selectedOutputType: "teaspoon",
+            selectedInputType: "teaspoon",
+            inputValue: "",
+            conversions: ["Brazilian"],
+            units: [
+                {
+                    name: "teaspoon",
+                    type: "liquid",
+                    conversions: [
+                        {
+                            inputType: "teaspoon",
+                            conversion: (x) => x
+                        },
+                        {
+                            inputType: "dessert spoon",
+                            conversion: (x) => x / 12
+                        },
+                        {
+                            inputType: "soup spoon",
+                            conversion: (x) => x * 3
+                        },
+                        {
+                            inputType: "cup (Brazilian)",
+                            conversion: (x) => x / 30.54
+                        },
+                        
+
+                    ]
+                },
+                
+
+            ]
+        }
+    ]);
     const [Units, setUnits] = useState([
         { title: "Citizen Kane", director: "Orson Welles", year: "1941", id: 1 },
         { title: "The Waterboy", director: "Frank Coraci", year: "1998", id: 2 },
