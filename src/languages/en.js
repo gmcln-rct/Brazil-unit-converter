d5ad16c47af28e5d66053aec6e32426f34d1fbff
Glenn McClanan <producersama@gmail.com>
2020-02-23 13:36:45 -0500

    Add English dictionary

--- src/languages/en.js
+++ src/languages/en.js
@@ -1,9 +1,13 @@
 const en = {
-    exploreHeader: 'Explore',
-    welcomeDescription: 'This is a demo app for multi-language website with React Context API',
-    clickMe: 'Click Me',
-    aboutMe: 'For more info about the author',
-    buttonClicked: 'You clicked to button!'
+    tspbz: 'Teaspoon (BZ)',
+    dspbz: 'Dessert Spoon (BZ)',
+    sspbz: 'Soup Spoon (BZ)',
+    teacupbz: 'Teacup (BZ)',
+    cupbz: 'Cup (BZ)',
+    tspus: 'Teaspoon (US)',
+    tbspus: 'Tablespoon (US)',
+    cupus: 'Cup (US)',
+    ml: 'Milliliter',
 };
 
 export default en;
