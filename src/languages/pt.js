b788ec206945184036bd20cdf32bc710c09b690f
Glenn McClanan <producersama@gmail.com>
2020-02-23 13:35:57 -0500

    Add portuguese dictionary

---
+++ src/languages/pt.js
@@ -0,0 +1,13 @@
+const pt = {
+    tspbz: 'colher de chá ',
+    dspbz: 'colher de sobremesa ',
+    sspbz: 'colher de sopa ',
+    teacupbz: 'xícara ',
+    cupbz: 'copo de requeijão ',
+    tspus: 'colher de chá (EUA)',
+    tbspus: 'colher de sopa (EUA',
+    cupus: 'copo (EUA)',
+    ml: 'Mililitro',
+};
+
+export default pt;
