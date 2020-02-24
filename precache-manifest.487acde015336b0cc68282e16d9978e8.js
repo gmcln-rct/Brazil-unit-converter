e9cad463c53506aae3b0103f84fd23c1c4bbbf47
Glenn McClanan <producersama@gmail.com>
2020-02-23 22:38:19 -0500

    Updates

--- precache-manifest.487acde015336b0cc68282e16d9978e8.js
+++ precache-manifest.487acde015336b0cc68282e16d9978e8.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "60ede8c069ff553b3cccbd62ccb06df5",
+    "revision": "560002d308dfe611dd6879fd26561b8e",
     "url": "/Brazil-unit-converter/index.html"
   },
   {
-    "revision": "79334b9567ef219d9b67",
-    "url": "/Brazil-unit-converter/static/css/main.d9293b2e.chunk.css"
+    "revision": "412065672c9d674925f7",
+    "url": "/Brazil-unit-converter/static/css/main.a2a73e58.chunk.css"
   },
   {
     "revision": "7b1a36c1470c75511d61",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Brazil-unit-converter/static/js/2.535adac6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "79334b9567ef219d9b67",
-    "url": "/Brazil-unit-converter/static/js/main.feec2c9e.chunk.js"
+    "revision": "412065672c9d674925f7",
+    "url": "/Brazil-unit-converter/static/js/main.4d2828e2.chunk.js"
   },
   {
     "revision": "06ede6b19efb522015ce",
