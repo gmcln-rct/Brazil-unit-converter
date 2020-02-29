edf887ef45da15fd8a4d9ba6b7adff4c94e2c771
Glenn McClanan <producersama@gmail.com>
2020-02-29 14:33:01 -0500

    Updates

---
+++ precache-manifest.bb13485cbe87f209dd98ff0b8f3dac30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5170beee3c9c40270040f4c6389d80f",
+    "url": "/Brazil-unit-converter/index.html"
+  },
+  {
+    "revision": "092425eace7859b4e302",
+    "url": "/Brazil-unit-converter/static/css/main.fc5dcc6c.chunk.css"
+  },
+  {
+    "revision": "7b1a36c1470c75511d61",
+    "url": "/Brazil-unit-converter/static/js/2.535adac6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Brazil-unit-converter/static/js/2.535adac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092425eace7859b4e302",
+    "url": "/Brazil-unit-converter/static/js/main.1d485b53.chunk.js"
+  },
+  {
+    "revision": "06ede6b19efb522015ce",
+    "url": "/Brazil-unit-converter/static/js/runtime-main.8dfed327.js"
+  }
+]);
