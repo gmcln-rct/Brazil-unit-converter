d67be3861b61f98cf0ce7b0bacab615ca11adf94
Glenn McClanan <producersama@gmail.com>
2020-03-08 16:55:50 -0400

    Stremline header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,11 @@ const Header = () => {
         <div className = "header-section">
             
             <img src='purpleflag.svg' alt="Copo e colher - Brazilian Unit Converter logo" className='logo'/>
-
                 
             <h1>Brazilian Recipe Unit Converter</h1>
 
         </div>
     )
-
 
 };
 
