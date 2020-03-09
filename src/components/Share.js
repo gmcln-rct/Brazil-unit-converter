6e3498e394f5145e600d34bae72873975d027b9b
Glenn McClanan <producersama@gmail.com>
2020-03-08 20:15:53 -0400

    Add share component

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,7 +1,28 @@
+import React from 'react';
+
 import {
   EmailShareButton
   FacebookShareButton,
-    TwitterShareButton,
+TwitterShareButton,
 
 } from "react-share";
 
+const ShareSelect = () => {
+    
+
+
+
+
+    return (
+        <section className='share-select'>
+ 
+
+            <FacebookShareCount url={shareUrl} />
+
+        </section>
+
+    );
+};
+
+
+export default ShareSelect;
