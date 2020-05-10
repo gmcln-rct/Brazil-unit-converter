b56d24da3ae9e225ab0ac2074337e6f8bcb88665
gmcln-rct <producersama@gmail.com>
2020-05-09 21:22:58 -0400

    Update subheader text

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
             {/* <h2>Convert between Brazilian and U.S./Metric Measurement</h2> */}
             <details>
                 <summary>Convert between Brazilian and U.S./Metric Measurement</summary>
-                <p>The units of measurement used in Brazil are different than in the U.S. and Europe, often causing confusion when converting recipes. This app address this problem by enabling users to convert between different units of volume, in English or Portuguese</p>
+                <p>The units of measurement used for recipes in Brazil are different than in the U.S. and Europe, often causing confusion when converting recipes. This app address this problem by enabling users to convert between different units of volume, in English or Portuguese</p>
                 <p>Developed by Glenn McClanan with React Hooks and Context API.</p>
 
             </details>
