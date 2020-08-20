dde4ff8c9e3807c1c0460f21a4f68c86b6b56c50
gmcln-rct <producersama@gmail.com>
2020-08-20 18:53:04 -0400

    Updates

---
+++ precache-manifest.dcee6898285227b92646c127c120c8eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c329d3e101543580c917b6cd6ce955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc2e784931ab57d02a77",
+    "url": "/static/css/main.0dc8e578.chunk.css"
+  },
+  {
+    "revision": "76378031ce4de996e2fc",
+    "url": "/static/js/2.d7257aca.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/static/js/2.d7257aca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc2e784931ab57d02a77",
+    "url": "/static/js/main.93464e80.chunk.js"
+  },
+  {
+    "revision": "aeb04609f464170003c5",
+    "url": "/static/js/runtime-main.b2a3c672.js"
+  }
+]);
