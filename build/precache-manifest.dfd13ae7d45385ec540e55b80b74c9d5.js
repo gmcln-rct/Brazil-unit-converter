89c40f6b1f22f79252bf623a391e4fad53a5192b
gmcln-rct <producersama@gmail.com>
2020-08-20 20:57:45 -0400

    Run deploy

--- build/precache-manifest.dfd13ae7d45385ec540e55b80b74c9d5.js
+++ build/precache-manifest.dfd13ae7d45385ec540e55b80b74c9d5.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e5c329d3e101543580c917b6cd6ce955",
+    "revision": "72b6a9097200b169a93443cb4d71c60b",
     "url": "/index.html"
   },
   {
-    "revision": "dc2e784931ab57d02a77",
-    "url": "/static/css/main.0dc8e578.chunk.css"
+    "revision": "4ab195ece387e156f05d",
+    "url": "/static/css/main.d2c1ee28.chunk.css"
   },
   {
     "revision": "76378031ce4de996e2fc",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.d7257aca.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dc2e784931ab57d02a77",
-    "url": "/static/js/main.93464e80.chunk.js"
+    "revision": "4ab195ece387e156f05d",
+    "url": "/static/js/main.0b60f295.chunk.js"
   },
   {
     "revision": "aeb04609f464170003c5",
